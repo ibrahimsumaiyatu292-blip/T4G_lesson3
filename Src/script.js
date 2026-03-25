a79f54e32dd5771792f91a822423eb194b2fe74f
ibrahimsumaiyatu292-blip <ibrahimsumaiyatu292@gmail.com>
2026-03-25 10:23:26 -0400

    Add everything

---
+++ Src/script.js
@@ -0,0 +1,32 @@
+let name = "Sumaiyatu";
+console.log("Sumaiyatu");
+
+let age = 20;
+console.log(20);
+
+let num1 = 20 + 10;
+console.log(num1);
+let num2 = 10 - 8;
+console.log(num2);
+let num3 = 10 * 8;
+console.log(num3);
+let num4 = 10 / 25
+console.log(num4);
+
+let num5 = 15 % 4;
+console.log(15 % 4);
+
+let x = true;
+let y = false;
+console.log(x);
+console.log(y);
+
+let a = 10;
+typeof(a);
+console.log(typeof(a));
+
+let b = "Hello";
+typeof(b);
+console.log(typeof(b));
+
+
